75940995eb8967918186ccb85d09752ecba654e3
Federico Gabriel Bastias Cano <38295529+GabiBastias@users.noreply.github.com>
2023-08-30 11:59:46 -0300

    force deleted

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,6 @@ const { PORT } = process.env;
 const { conn } = require('./src/DB_connection');
 
 server.listen(PORT, async() => {
-   await conn.sync({ force: true });
+   await conn.sync();
    console.log('Server raised in port: ' + PORT);
 });  
