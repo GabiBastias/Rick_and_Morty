5c03047671a359c30693b93448825efe1cd560bf
Federico Gabriel Bastias Cano <38295529+GabiBastias@users.noreply.github.com>
2023-08-30 09:40:31 -0300

    conn.sync changed to force

--- Server/src/index.js
+++ Server/src/index.js
@@ -3,6 +3,6 @@ const PORT = 3001;
 const { conn } = require('../DB_connection');
 
 server.listen(PORT, async() => {
-   await conn.sync({alter: true});
+   await conn.sync({force: true});
    console.log('Server raised in port: ' + PORT);
 });  
